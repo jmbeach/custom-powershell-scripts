371b53188157c6a20d15acef0c117c8906263f8b
jmbeach <beachj@ironsolutions.com>
2020-01-31 07:56:44 -0600

    adds comment to filters

--- filters.ps1
+++ filters.ps1
@@ -1,2 +1,3 @@
+# can easily pass these to Where-Object to not get node_modules and things
 $FilterWebProject = [ScriptBlock]{ $_.Directory -NotLike "*node_modules*" -and $_.Directory -NotLike "*lib*" };
 $FilterDotNet = [ScriptBlock]{ $_.Directory -NotLike "*packages*" };
