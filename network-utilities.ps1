ac72aad2e39c3bb085ad8f9d6bde29416b81f797
jmbeach <beachj@ironsolutions.com>
2020-09-14 12:04:40 -0500

    scan ports

--- network-utilities.ps1
+++ network-utilities.ps1
@@ -2,3 +2,12 @@ function Get-ProcessByPort ($port) {
   $connection = Get-NetTCPConnection -LocalPort $port
   return Get-Process -Id $connection.OwningProcess
 }
+
+function Scan-PortsForProcesses($startingPort, $endingPort) {
+  for ($i = $startingPort; $i -le $endingPort; $i++) {
+    try {
+      Get-ProcessByPort $i
+    }
+    catch {}
+  }
+}
