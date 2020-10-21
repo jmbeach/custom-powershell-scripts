659ead112a7a498ac0e692c3c9333d52063d7048
jmbeach <beachj@ironsolutions.com>
2020-10-21 10:26:21 -0500

    parse all ssh config

--- ssh.ps1
+++ ssh.ps1
@@ -8,23 +8,41 @@ $sshCompleter = {
   }
 }
 
-function Get-SshHostNames() {
+function Get-SshHosts() {
   $sshConfig = Get-Content "$home/.ssh/config";
-  $result = [System.Collections.Generic.List[string]]::new();
+  $result = [System.Collections.Generic.List[object]]::new();
+  $activeHost = $null;
   $sshConfig | ForEach-Object {
     $line = $_.ToString();
-    if (-not $line.StartsWith(" ") -and $line.Length -gt 0) {
-      $result.Add($line.Split('Host ')[1]);
+    # if the line doesn't start with whitespace, it's a host
+    if ($line.TrimEnd() -eq $line.Trim() -and $line.Length -gt 0) {
+      if ($null -ne $activeHost) {
+        $result.Add($activeHost);
+      }
+
+      $activeHost = [psobject]::new();
+
+      $activeHost | Add-Member -NotePropertyName 'name' -NotePropertyValue $line.Split('Host ')[1];
+    } elseif ($line.Trim().StartsWith('User')) {
+      $activeHost | Add-Member -NotePropertyName 'user' -NotePropertyValue $line.Trim().Split('User ')[1];
+    } elseif ($line.Trim().StartsWith('Hostname')) {
+      $activeHost | Add-Member -NotePropertyName 'hostname' -NotePropertyValue $line.Trim().Split('Hostname ')[1];
+    } elseif ($line.Trim().StartsWith('Port')) {
+      $activeHost | Add-Member -NotePropertyName 'port' -NotePropertyValue $line.Trim().Split('Port ')[1];
+    } elseif ($line.Trim().StartsWith('IdentityFile')) {
+      $activeHost | Add-Member -NotePropertyName 'identityFile' -NotePropertyValue $line.Trim().Split('IdentityFile ')[1];
     }
+  }
+
+  if ($null -ne $activeHost) {
+    $result.Add($activeHost);
   }
 
   return $result;
 }
 
-get-SshHostNames
-
 function Complete-HostName($word) {
-  $hostNames = Get-SshHostNames;
+  $hostNames = (Get-SshHosts).name;
   $hostNames | Where-Object {
     $_.ToUpper() -like "*$($word.ToUpper())*"
   } | ForEach-Object {
