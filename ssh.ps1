617cd99e362d459157bafeafd02b3ed38db05a10
jmbeach <beachj@ironsolutions.com>
2020-12-30 20:12:43 -0600

    parse global properties

--- ssh.ps1
+++ ssh.ps1
@@ -21,6 +21,30 @@ class SshConfig {
   [string]$IdentityFile;
 }
 
+class SshPropertyParser {
+  [boolean]isProp($line, $propertyName) {
+    return $line.Trim().StartsWith($propertyName, 'CurrentCultureIgnoreCase');
+  }
+
+  [string]parse($line, $propertyName, $propertyType) {
+    $val = ($line.Trim() -split $propertyName)[1].Trim();
+    $typeName = [System.Text.RegularExpressions.RegEx]::new('(?:string|int32)').Matches($propertyType.ToLower())[0].Value;
+    switch ($typeName) {
+      'string' { 
+        return $val;
+      }
+      'int32' {
+        return [int]::Parse($val);
+      }
+      Default {
+        throw "Unexpected property type `"$typeName`"";
+      }
+    }
+
+    throw "Unknown error parsing property";
+  }
+}
+
 class SshHostParser {
   [System.IO.TextReader]$reader;
 
@@ -35,29 +59,18 @@ class SshHostParser {
 
   [SshHost]parse ([string]$line) {
     $sshHost = New-Object -TypeName SshHost -Property @{ Name = ($line.Trim() -split 'Host ')[1]; };
+    $propParser = [SshPropertyParser]::new();
 
     while ($this.isHostLine()) {
       $line = $this.reader.ReadLine();
-      $props = $sshHost.PSObject.Properties
+      $props = $sshHost.PSObject.Properties;
       $props | ForEach-Object {
         $prop = $_;
-        if (-not $line.Trim().StartsWith($prop.Name, 'CurrentCultureIgnoreCase')) {
+        if (-not $propParser.isProp($line, $prop.Name)) {
           return;
         }
 
-        $val = ($line.Trim() -split $prop.Name)[1].Trim();
-        $typeName = [System.Text.RegularExpressions.RegEx]::new('(?:string|int32)').Matches($prop.TypeNameOfValue.ToLower())[0].Value;
-        switch ($typeName) {
-          'string' { 
-            $prop.Value = $val;
-          }
-          'int32' {
-            $prop.Value = [int]::Parse($val);
-          }
-          Default {
-            throw "Unexpected property type `"$typeName`"";
-          }
-        }
+        $prop.Value = $propParser.parse($line, $prop.Name, $prop.TypeNameOfValue);
       }
     }
     
@@ -96,20 +109,27 @@ function ConvertFrom-SshConfigFile (
   # looping over the lines in the config file to incrementally build hosts/properties
   $reader = [System.IO.StreamReader]::new($sshFile);
   $hostParser = [SshHostParser]::new($reader);
+  $propParser = [SshPropertyParser]::new();
 
   while ($null -ne ($line = $reader.ReadLine())) {
-    # if the line doesn't start with whitespace, it's a host or entity
-    if ($line.TrimEnd() -eq $line.Trim() -and $line.Length -gt 0) {
-      if ($hostParser.isHostLine()) {
-        if ($null -eq $result.Hosts) {
-          $result.Hosts = [System.Collections.Generic.List[SshHost]]::new();
+    if ($hostParser.isHostLine()) {
+      if ($null -eq $result.Hosts) {
+        $result.Hosts = [System.Collections.Generic.List[SshHost]]::new();
         }
 
         $result.Hosts.Add($hostParser.parse($line));
         continue;
+    }
+
+    # if it's not a host line, it's a global setting
+    $props = $result.PSObject.Properties | Where-Object { $_.Name -ne 'Hosts' };
+    $props | ForEach-Object {
+      $prop = $_;
+      if (-not $propParser.isProp($line, $prop.Name)) {
+        return;
       }
 
-      # if it's not a host line, it's a global setting
+      $prop.Value = $propParser.parse($line, $prop.Name, $prop.TypeNameOfValue);
     }
   }
 
