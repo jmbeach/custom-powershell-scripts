dd9fb96a8d6dd22b4fb4684969933d5283fbcd71
jmbeach <beachj@ironsolutions.com>
2020-12-30 19:02:01 -0600

    improving robustness of ssh config parser

--- ssh.ps1
+++ ssh.ps1
@@ -4,49 +4,123 @@ $sshCompleter = {
   # is this the second parameter?
   $is2nd = 4 + $wordToComplete.length - $cursorPosition -eq 0;
   if ($is2nd) {
-    Complete-HostName $wordToComplete
+    _Complete-HostName $wordToComplete
   }
 }
 
-function Get-SshHosts($sshFile) {
+class SshHost {
+  [string]$Name;
+  [string]$User;
+  [string]$HostName;
+  [int]$Port;
+  [string]$IdentityFile
+}
+
+class SshConfig {
+
+}
+
+function ConvertFrom-SshConfigFile (
+  <#
+   .PARAMETER sshFile
+     Path to ssh config file.
+     If null, defaults to ~/.ssh/config
+  #>
+  $sshFile
+) {
+  <#
+    .SYNOPSIS
+      Converts an ssh config file into a list of powershell objects;
+      each representing a host in the config.
+    
+    .DESCRIPTION
+      Converts an ssh config file into a list of powershell objects
+    
+    .OUTPUTS
+      Returns a list of powershell objects;
+      each representing a host in the config.
+      Null if no sshFile specified and the default path for one does not exist.
+  #>
   if ($null -eq $sshFile) {
     $sshFile = "$home/.ssh/config";
   }
 
-  $sshConfig = Get-Content $sshFile;
-  $result = [System.Collections.Generic.List[object]]::new();
-  $activeHost = $null;
-  $sshConfig | ForEach-Object {
-    $line = $_.ToString();
-    # if the line doesn't start with whitespace, it's a host
+  if (-not (Test-Path $sshFile)) {
+    return $null
+  }
+
+  $result = [System.Collections.Generic.List[SshHost]]::new();
+  $sshHost = $null;
+
+  # looping over the lines in the config file to incrementally build hosts/properties
+  $reader = [System.IO.StreamReader]::new($sshFile)
+
+  while ($null -ne ($line = $reader.ReadLine())) {
+    # if the line doesn't start with whitespace, it's a host or entity
     if ($line.TrimEnd() -eq $line.Trim() -and $line.Length -gt 0) {
-      if ($null -ne $activeHost) {
-        $result.Add($activeHost);
+      # Add the previously built entity if one exists
+      if ($null -ne $sshHost) {
+        $result.Add($sshHost);
       }
 
-      $activeHost = [psobject]::new();
+      $sshHost = New-Object -TypeName SshHost -Property @{ Name = ($line.Trim() -split 'Host ')[1]; };
 
-      $activeHost | Add-Member -NotePropertyName 'name' -NotePropertyValue ($line.Trim() -split 'Host ')[1];
-    } elseif ($line.Trim().StartsWith('User', 'CurrentCultureIgnoreCase')) {
-      $activeHost | Add-Member -NotePropertyName 'user' -NotePropertyValue ($line.Trim() -split 'User ')[1];
-    } elseif ($line.Trim().StartsWith('Hostname', 'CurrentCultureIgnoreCase')) {
-      $activeHost | Add-Member -NotePropertyName 'hostname' -NotePropertyValue ($line.Trim() -split 'Hostname ')[1];
-    } elseif ($line.Trim().StartsWith('Port', 'CurrentCultureIgnoreCase')) {
-      $activeHost | Add-Member -NotePropertyName 'port' -NotePropertyValue ($line.Trim() -split 'Port ')[1];
-    } elseif ($line.Trim().StartsWith('IdentityFile', 'CurrentCultureIgnoreCase')) {
-      $activeHost | Add-Member -NotePropertyName 'identityFile' -NotePropertyValue ($line.Trim() -split 'IdentityFile ')[1];
+      # go straight to next line
+      continue;
+    }
+
+    # Helper method for detecting properties of SshHosts
+    function isProp([string]$propName) {
+      return $line.Trim().StartsWith($propName, 'CurrentCultureIgnoreCase');
+    }
+
+    function parseProperty([string]$property) {
+      return ($line.Trim() -split $property)[1].Trim();
+    }
+
+    $props = $sshHost.PSObject.Properties
+    $props | ForEach-Object {
+      $prop = $_;
+      if (-not (isProp -propName $prop.Name)) {
+        return;
+      }
+
+      $val = parseProperty -property $prop.Name;
+      $typeName = $prop.TypeNameOfValue.ToLower().Replace('system.', '');
+      switch ($typeName) {
+        'string' { 
+          $prop.Value = $val;
+        }
+        'int32' {
+          $prop.Value = [int]::Parse($val);
+        }
+        Default {
+          throw "Unexpected property type `"$typeName`"";
+        }
+      }
     }
   }
 
-  if ($null -ne $activeHost) {
-    $result.Add($activeHost);
+  <# The last host has to be added manually because the other hosts
+     are added at the top of the loop #>
+  if ($null -ne $sshHost) {
+    $result.Add($sshHost);
   }
 
   return $result;
 }
 
-function Complete-HostName($word) {
-  $hostNames = (Get-SshHosts).name;
+function _Complete-HostName() {
+  # _ = private. TODO: Change if I decide to make this into a module and just don't export this function
+  [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '', Scope = 'Function')]
+  [CmdletBinding()]
+  param (
+    [Parameter()]
+    [string]
+    $word
+  )
+
+  $hostNames = (ConvertFrom-SshConfigFile).name;
   $hostNames | Where-Object {
     $_.ToUpper() -like "*$($word.ToUpper())*"
   } | ForEach-Object {
@@ -56,13 +130,33 @@ function Complete-HostName($word) {
 
 Register-ArgumentCompleter -Native -CommandName ssh -ScriptBlock $sshCompleter;
 
-# Making an exception for bad naming here because it's an alias for the actual unix command
 # Inspired by https://www.chrisjhart.com/Windows-10-ssh-copy-id/
-function Ssh-Copy-Id($i, $target) {
-  if (![System.IO.File]::Exists($i)) {
-    Write-Host -ForegroundColor Red -Object "Input file doesn't exist.";
+function Ssh-Copy-Id {
+  # Making an exception for bad naming here because it's an alias for the actual unix command
+  [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '', Scope = 'Function')]
+  [CmdletBinding()]
+  param (
+    [Parameter(Required = $true)][string] $target,
+    <#
+       .PARAMETER identityFile
+         Name/path to the identity file you want to copy to the host.
+         Defaults to ~/.ssh/id_rsa.pub
+      #>
+    [Parameter(Required = $false)][string] $identityFile
+  )
+
+  if ($null -eq $identityFile) {
+    $identityFile = "$($env:USERPROFILE)\.ssh\id_rsa.pub"
+  }
+
+  if (!$identityFile.EndsWith('.pub')) {
+    $identityFile = "$identityFile.pub"
+  }
+
+  if (![System.IO.File]::Exists($identityFile)) {
+    Write-Host -ForegroundColor Red -Object "Identity file specified doesn't exist: `"$identityFile`".";
     return;
   }
 
-  Get-Content $i | ssh $target "cat >> .ssh/authorized_keys";
-}
+  Get-Content $identityFile | ssh $target "cat >> .ssh/authorized_keys";
+}
