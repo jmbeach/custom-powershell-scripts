73dd626623ec7ca80ac313ce3ec87b260c4bf722
jmbeach <beachj@ironsolutions.com>
2020-12-30 19:33:39 -0600

    adds host parser

--- ssh.ps1
+++ ssh.ps1
@@ -20,6 +20,50 @@ class SshConfig {
 
 }
 
+class SshHostParser {
+  [System.IO.TextReader]$reader;
+
+  SshHostParser([System.IO.TextReader]$reader) {
+    $this.reader = $reader;
+  }
+
+  [boolean]_isHostLine() {
+    # space = 32. If the line starts with whitespace, it's a host property
+    return 32 -eq $this.reader.Peek();
+  }
+
+  [SshHost]parse ([string]$line) {
+    $sshHost = New-Object -TypeName SshHost -Property @{ Name = ($line.Trim() -split 'Host ')[1]; };
+
+    while ($this._isHostLine()) {
+      $line = $this.reader.ReadLine();
+      $props = $sshHost.PSObject.Properties
+      $props | ForEach-Object {
+        $prop = $_;
+        if (-not $line.Trim().StartsWith($prop.Name, 'CurrentCultureIgnoreCase')) {
+          return;
+        }
+
+        $val = ($line.Trim() -split $prop.Name)[1].Trim();
+        $typeName = $prop.TypeNameOfValue.ToLower().Replace('system.', '');
+        switch ($typeName) {
+          'string' { 
+            $prop.Value = $val;
+          }
+          'int32' {
+            $prop.Value = [int]::Parse($val);
+          }
+          Default {
+            throw "Unexpected property type `"$typeName`"";
+          }
+        }
+      }
+    }
+    
+    return $sshHost;
+  }
+}
+
 function ConvertFrom-SshConfigFile (
   <#
    .PARAMETER sshFile
@@ -50,61 +94,16 @@ function ConvertFrom-SshConfigFile (
   }
 
   $result = [System.Collections.Generic.List[SshHost]]::new();
-  $sshHost = $null;
 
   # looping over the lines in the config file to incrementally build hosts/properties
-  $reader = [System.IO.StreamReader]::new($sshFile)
+  $reader = [System.IO.StreamReader]::new($sshFile);
+  $hostParser = [SshHostParser]::new($reader);
 
   while ($null -ne ($line = $reader.ReadLine())) {
     # if the line doesn't start with whitespace, it's a host or entity
     if ($line.TrimEnd() -eq $line.Trim() -and $line.Length -gt 0) {
-      # Add the previously built entity if one exists
-      if ($null -ne $sshHost) {
-        $result.Add($sshHost);
-      }
-
-      $sshHost = New-Object -TypeName SshHost -Property @{ Name = ($line.Trim() -split 'Host ')[1]; };
-
-      # go straight to next line
-      continue;
+      $result.Add($hostParser.parse($line));
     }
-
-    # Helper method for detecting properties of SshHosts
-    function isProp([string]$propName) {
-      return $line.Trim().StartsWith($propName, 'CurrentCultureIgnoreCase');
-    }
-
-    function parseProperty([string]$property) {
-      return ($line.Trim() -split $property)[1].Trim();
-    }
-
-    $props = $sshHost.PSObject.Properties
-    $props | ForEach-Object {
-      $prop = $_;
-      if (-not (isProp -propName $prop.Name)) {
-        return;
-      }
-
-      $val = parseProperty -property $prop.Name;
-      $typeName = $prop.TypeNameOfValue.ToLower().Replace('system.', '');
-      switch ($typeName) {
-        'string' { 
-          $prop.Value = $val;
-        }
-        'int32' {
-          $prop.Value = [int]::Parse($val);
-        }
-        Default {
-          throw "Unexpected property type `"$typeName`"";
-        }
-      }
-    }
-  }
-
-  <# The last host has to be added manually because the other hosts
-     are added at the top of the loop #>
-  if ($null -ne $sshHost) {
-    $result.Add($sshHost);
   }
 
   return $result;
