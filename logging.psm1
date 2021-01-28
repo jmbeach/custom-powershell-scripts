77a17b9c2ae993408685a0b5a8a7d0183c1d285c
jmbeach <beachj@ironsolutions.com>
2021-01-28 11:59:17 -0600

    adds file logger and multi-target logger

--- logging.psm1
+++ logging.psm1
@@ -11,33 +11,95 @@ class IWriteLogParams {
 }
 class ILogger {
   [void] WriteLog([IWriteLogParams]$logParams) {}
+  [void] WriteError([IWriteLogParams]$logParams) {}
 }
 
 class ConsoleLogger : ILogger {
-  [void] WriteLog([IWriteLogParams]$logParams) {
+  [void] _writeLog([IWriteLogParams]$logParams, [string]$logType) {
     if ($null -eq $logParams) {
       Write-Host;
       return;
     }
 
-    [scriptBlock]$doWrite = {
-      if ($null -eq $logParams.ForegroundColor -and $null -eq $logParams.BackgroundColor) {
-        Write-Host -Object $logParams.Object -NoNewline -Separator $logParams.Separator;
-      }
-      elseif ($null -eq $logParams.ForegroundColor) {
-        Write-Host -Object $logParams.Object -NoNewline -Separator $logParams.Separator -BackgroundColor $logParams.BackgroundColor;
-      }
-      elseif ($null -eq $logParams.BackgroundColor) {
-        Write-Host -Object $logParams.Object -NoNewline -Separator $logParams.Separator -ForegroundColor $logParams.ForegroundColor;
-      }
-      else {
-        Write-Host -Object $logParams.Object -NoNewline -Separator $logParams.Separator -ForegroundColor $logParams.ForegroundColor -BackgroundColor $logParams.BackgroundColor;
-      }
+    if ($null -eq $logParams.ForegroundColor -and $null -eq $logParams.BackgroundColor) {
+      Write-Host -Object $logParams.Object -NoNewline -Separator $logParams.Separator;
+    }
+    elseif ($null -eq $logParams.ForegroundColor) {
+      Write-Host -Object $logParams.Object -NoNewline -Separator $logParams.Separator -BackgroundColor $logParams.BackgroundColor;
+    }
+    elseif ($null -eq $logParams.BackgroundColor) {
+      Write-Host -Object $logParams.Object -NoNewline -Separator $logParams.Separator -ForegroundColor $logParams.ForegroundColor;
+    }
+    else {
+      Write-Host -Object $logParams.Object -NoNewline -Separator $logParams.Separator -ForegroundColor $logParams.ForegroundColor -BackgroundColor $logParams.BackgroundColor;
     }
 
-    Invoke-Command $doWrite;
     if (-not $logParams.NoNewLine) {
       Write-Host;
+    }
+  }
+
+  [void] WriteLog([IWriteLogParams]$logParams) {
+    $this._writeLog($logParams, "INFO");
+  }
+
+  [void] WriteError([IWriteLogParams]$logParams) {
+    $this._writeLog($logParams, "ERROR");
+  }
+}
+
+class FileLogger: ILogger {
+  [string]$file;
+  FileLogger($file) {
+    $this.file = $file;
+  }
+
+  [void] _writeLog([IWriteLogParams]$logParams, [string]$logType) {
+    if ($null -eq $logParams) {
+      return;
+    }
+
+    $logText = $logParams.Object;
+    if ($logParams.Object.GetType().Name -like '*[]*' -and $null -ne $logParams.Separator) {
+      $logText = [string]::Join($logParams.Separator, $logParams.Object);
+    }
+
+    $arguments = @{
+      Append=$true;
+      FilePath=$this.file;
+    }
+
+    if ($logParams.NoNewLine) {
+      $arguments.Add("NoNewLine", $logParams.NoNewLine);
+    }
+
+    "$([System.DateTime]::Now.ToString("MM-dd-yyyy HH:mm:ss")) - $logType - $logText" | Out-File @arguments;
+  }
+
+  [void] WriteLog([IWriteLogParams]$logParams) {
+    $this._writeLog($logParams, "INFO");
+  }
+
+  [void] WriteError([IWriteLogParams]$logParams) {
+    $this._writeLog($logParams, "ERROR");
+  }
+}
+
+class MultiTargetLogger: ILogger {
+  [ILogger[]]$loggers;
+  MultiTargetLogger([ILogger[]]$loggers) {
+    $this.loggers = $loggers;
+  }
+
+  [void] WriteLog([IWriteLogParams]$logParams) {
+    $this.loggers | ForEach-Object {
+      $_.WriteLog($logParams);
+    }
+  }
+
+  [void] WriteError([IWriteLogParams]$logParams) {
+    $this.loggers | ForEach-Object {
+      $_.WriteError($logParams);
     }
   }
 }
