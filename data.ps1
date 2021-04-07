84e13daffcde9825c46af83b3563cfa3e5f78f97
jmbeach <beachj@ironsolutions.com>
2021-04-07 12:43:09 -0500

    convertto-yaml

--- data.ps1
+++ data.ps1
@@ -11,3 +11,29 @@ function Convert-ObjectToNestedHash ($obj) {
 
   return $result;
 }
+
+function ConvertTo-Yaml {
+  <#
+    .SYNOPSIS
+      Converts objects in pipeline to yaml
+  
+    .DESCRIPTION
+      Uses YamlDotNet behind the scenes
+  
+    .INPUTS
+      A powershell object to convert to yaml
+  
+    .OUTPUTS
+      A yaml string
+  
+    .EXAMPLE
+      $myObject | ConvertTo-Yaml
+  #>
+  [CmdletBinding()]
+  param (
+    [Parameter(ValueFromPipeline)]$Object
+  )
+  Import-Module "$home\.nuget\YamlDotNet.11.0.1\lib\netstandard2.1\YamlDotNet.dll"
+  $serializer = [YamlDotNet.Serialization.SerializerBuilder]::new().Build()
+  return $serializer.Serialize($Object)
+}
