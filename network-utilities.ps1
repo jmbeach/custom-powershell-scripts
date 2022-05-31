06903d92ef6eba135c7199c78106439709b71a33
jmbeach <jaredbeachdesign@gmail.com>
2022-05-31 16:01:18 -0400

    get-processbyport for mac

--- network-utilities.ps1
+++ network-utilities.ps1
@@ -1,6 +1,14 @@
 function Get-ProcessByPort ($port) {
-  $connection = Get-NetTCPConnection -LocalPort $port
-  return Get-Process -Id $connection.OwningProcess
+  if ($IsMacOS) {
+    $result = netstat -vanp tcp | Select-String $port;
+    $isInt = $false;
+    $numbers = $result -split '\s+' | Where-Object { [Int]::TryParse($_, [ref]$isInt) -and $_ -ne '0' }
+    $foundPid = $numbers[-1];
+    return Get-Process -Id $foundPid
+  } else {
+    $connection = Get-NetTCPConnection -LocalPort $port
+    return Get-Process -Id $connection.OwningProcess
+  }
 }
 
 function Scan-PortsForProcesses($startingPort, $endingPort) {
