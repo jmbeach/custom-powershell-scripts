f0baac33463ac75f0e99e0b55d207d2ffb735a39
jmbeach <jaredbeachdesign@gmail.com>
2023-05-19 16:09:10 -0400

    print paid resources

--- aws.psm1
+++ aws.psm1
@@ -1,3 +1,28 @@
+function Parse-ResourceArn([string]$arn) {
+  $parts = $arn.Split(':')
+  $resourceParts = $parts[5].Split('/')
+  return [PSCustomObject]@{
+    Service = $parts[2];
+    Region = $parts[3];
+    ResourceType = $resourceParts[0]
+    ResourceId = $resourceParts[$resourceParts.Length - 1]
+  }
+}
+function Get-AwsFreeResources() {
+  return @(
+    [PSCustomObject]@{ Service = 'ec2'; ResourceType = 'dhcp-options' },
+    [PSCustomObject]@{ Service = 'ec2'; ResourceType = 'internet-gateway' },
+    [PSCustomObject]@{ Service = 'ec2'; ResourceType = 'network-acl' },
+    [PSCustomObject]@{ Service = 'ec2'; ResourceType = 'network-interface' },
+    [PSCustomObject]@{ Service = 'ec2'; ResourceType = 'route-table' },
+    [PSCustomObject]@{ Service = 'ec2'; ResourceType = 'security-group' },
+    [PSCustomObject]@{ Service = 'ec2'; ResourceType = 'subnet' },
+    [PSCustomObject]@{ Service = 'ec2'; ResourceType = 'vpc' },
+    [PSCustomObject]@{ Service = 'elasticloadbalancing'; ResourceType = 'targetgroup' },
+    [PSCustomObject]@{ Service = 'iam'; ResourceType = 'instance-profile' },
+    [PSCustomObject]@{ Service = 'iam'; ResourceType = 'policy' }
+  )
+}
 function awsall() {
   $regions = @(
     'ap-south-1',
@@ -26,4 +51,20 @@ function awsall() {
     $result.Add($commandResult)
   }
   return $result
+}
+
+function Get-AwsAllTaggedResources() {
+  awsall resourcegroupstaggingapi get-resources
+}
+
+function Get-AwsAllPaidResources($resources) {
+  $free = Get-AwsFreeResources
+  if ($null -eq $resources) {
+    $resources = Get-AwsAllTaggedResources
+  }
+  return $resources.ResourceTagMappingList | Where-Object {
+    $parsed = Parse-ResourceArn $_.ResourceArn
+    $match = $free | Where-Object { $_.Service -eq $parsed.Service -and $_.ResourceType -eq $parsed.ResourceType }
+    return $null -eq $match
+  }
 }
