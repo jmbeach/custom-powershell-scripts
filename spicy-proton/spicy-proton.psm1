2f545638679caff0ed68c3e84accac6450343193
jmbeach <jaredbeachdesign@gmail.com>
2024-01-15 11:46:45 -0500

    Refactor Get-AdjectiveNoun function to improve readability and remove duplicate code

--- spicy-proton/spicy-proton.psm1
+++ spicy-proton/spicy-proton.psm1
@@ -1,12 +1,13 @@
+$scriptPath = $PSScriptRoot
+$adjectives = Get-Content "$scriptPath/adjectives.txt"
+$nouns = Get-Content "$scriptPath/nouns.txt"
+$rand = [System.Random]::new()
+
 function Get-AdjectiveNoun {
     <#
       .SYNOPSIS
         Gets an adjective noun string. Mimics spicy-proton https://github.com/schmich/spicy-proton/tree/master/corpus
     #>
-    $rand = [System.Random]::new()
-    $scriptPath = $PSScriptRoot
-    $adjectives = Get-Content "$scriptPath/adjectives.txt"
-    $nouns = Get-Content "$scriptPath/nouns.txt"
     $adjectiveIndex = $rand.Next(0, $adjectives.Length)
     $nounIndex = $rand.Next(0, $nouns.Length)
     return @($adjectives[$adjectiveIndex], $nouns[$nounIndex])
